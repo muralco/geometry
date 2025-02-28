61671a2cd6ecf8c2ee73ef8ac006768b97423d10
Hernan Garchtrom <hernang87@gmail.com>
2025-02-28 09:30:28 -0300

    Rename WidgetObb to Obb

--- src/obb.ts
+++ src/obb.ts
@@ -2,7 +2,7 @@
  * Widget Oriented Bounding Box
  * Representation of a Widget coordinate system inside Mural
  */
-export type WidgetObb = {
+export type Obb = {
   size: { height: number; width: number };
   space: { cosR: number; origin: { x: number; y: number }; sinR: number }[];
 };
